8276aec066b2267b87cb9dab13d1f0985daebc8b
Priscila Finkler <prifinkler@gmail.com>
2024-10-11 15:04:57 +0100

    added interactivity to order items

--- restaurant-ordering/index.js
+++ restaurant-ordering/index.js
@@ -1,7 +1,18 @@
 import { menuArray } from '/data.js'
+const orderSection = document.getElementById('order-section')
+const orders = []
 
-function getItemsHtml() {
-  return menuArray.map(item => {
+document.addEventListener('click', function(e){
+  if (e.target.dataset.add) {
+    handleAddClick(e.target.dataset.add)
+  } else if (e.target.dataset.remove) {
+    handleRemoveClick(e.target.dataset.remove)
+  }
+  toggleOrderSection()
+})
+
+function getMenuHtml(menuArr) {
+  return menuArr.map(item => {
     const { name, ingredients, id, price, emoji } = item
     return `
           <li class="item-block">
@@ -11,10 +22,57 @@ function getItemsHtml() {
               <p class="ingredients">${ingredients.join(', ')}</p>
               <p class="price">£${price}</p>
             </div>
-            <button id="add-btn" class="add-btn"><i class="fa-solid fa-plus"></i></button>
+            <button id="add-btn" class="add-btn" data-add=${id}>
+              <i class="fa-solid fa-plus" data-add="${id}"></i>
+            </button>
           </li>
     `
   }).join('')
 }
 
-document.getElementById('menu-items').innerHTML = getItemsHtml()
+function handleAddClick(itemId){
+  const index = Number(itemId)
+  orders.push(menuArray[index])
+  return displayOrder()
+}
+
+function handleRemoveClick(itemId) {
+  const index = menuArray.indexOf(Number(itemId))
+  orders.splice(index, 1)
+  return displayOrder()
+}
+
+function toggleOrderSection(){
+  if (orders.length > 0) {
+    orderSection.classList.remove('hidden')
+  } else {
+    orderSection.classList.add('hidden')
+  }
+}
+
+function displayOrder() {
+  const orderList = orders.map(item => {
+    return `
+        <li class="order-item-info">
+          <h3>${item.name}</h3>
+          <button id="remove-btn" data-remove=${item.id}>remove</button>
+          <p>£${item.price}</p>
+        </li>
+    `
+  }).join('')
+  displayOrderTotal(orders)
+  return document.getElementById('order-items').innerHTML = orderList
+}
+
+function displayOrderTotal(orderArr) {
+  const total = orderArr.reduce((total, currentItem) => {
+    return total + currentItem.price
+  }, 0)
+  return document.querySelector('#order-total p').textContent = `£${total}`
+}
+
+function render(){
+  document.getElementById('menu-items').innerHTML = getMenuHtml(menuArray)
+}
+
+render()
