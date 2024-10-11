b497efd7e424ae0f8ec2d5915cbdc9d09df65ea8
Priscila Finkler <prifinkler@gmail.com>
2024-10-11 19:10:12 +0100

    added quantity and changed orders to hash type

--- restaurant-ordering/index.js
+++ restaurant-ordering/index.js
@@ -1,15 +1,48 @@
 import { menuArray } from '/data.js'
+
 const orderSection = document.getElementById('order-section')
-const orders = []
+const instructionsSection = document.getElementById('instructions-section')
+const orderItemsEl = document.getElementById('order-items')
+const orderTotalEl = document.querySelector('#order-total p')
+// const purchaseBtn = document.getElementById('purchase-btn')
+
+const orders = {}
 
 document.addEventListener('click', function(e){
-  if (e.target.dataset.add) {
-    handleAddClick(e.target.dataset.add)
-  } else if (e.target.dataset.remove) {
-    handleRemoveClick(e.target.dataset.remove)
+  const { add, remove } = e.target.dataset
+  if (add) {
+    handleAddClick(Number(add))
+  } else if (remove) {
+    handleRemoveClick(Number(remove))
   }
   toggleOrderSection()
 })
+
+function handleAddClick(itemId){
+  if (orders[itemId]) {
+    orders[itemId].quantity++
+  } else {
+    const item = menuArray.find(item => item.id === itemId)
+    orders[itemId] = {...item, quantity: 1 }
+  }
+  return displayOrder()
+}
+
+function handleRemoveClick(itemId) {
+  if (orders[itemId]) {
+    orders[itemId].quantity--
+    if (orders[itemId].quantity === 0) {
+      delete orders[itemId]
+    }
+  }
+  return displayOrder()
+}
+
+function toggleOrderSection(){
+  const hasOrders = Object.keys(orders).length > 0
+  orderSection.classList.toggle('hidden', !hasOrders)
+  instructionsSection.classList.toggle('hidden', hasOrders)
+}
 
 function getMenuHtml(menuArr) {
   return menuArr.map(item => {
@@ -30,49 +63,34 @@ function getMenuHtml(menuArr) {
   }).join('')
 }
 
-function handleAddClick(itemId){
-  const index = Number(itemId)
-  orders.push(menuArray[index])
-  return displayOrder()
-}
-
-function handleRemoveClick(itemId) {
-  const index = menuArray.indexOf(Number(itemId))
-  orders.splice(index, 1)
-  return displayOrder()
-}
-
-function toggleOrderSection(){
-  if (orders.length > 0) {
-    orderSection.classList.remove('hidden')
-  } else {
-    orderSection.classList.add('hidden')
-  }
-}
-
 function displayOrder() {
-  const orderList = orders.map(item => {
+  const orderList = Object.values(orders).map(item => {
     return `
         <li class="order-item-info">
-          <h3>${item.name}</h3>
+          <h3>${item.name} x ${item.quantity}</h3>
           <button id="remove-btn" data-remove=${item.id}>remove</button>
-          <p>£${item.price}</p>
+          <p>£${item.price * item.quantity}</p>
         </li>
     `
   }).join('')
-  displayOrderTotal(orders)
-  return document.getElementById('order-items').innerHTML = orderList
+
+  orderItemsEl.innerHTML = orderList
+  updateOrderTotal()
 }
 
-function displayOrderTotal(orderArr) {
-  const total = orderArr.reduce((total, currentItem) => {
-    return total + currentItem.price
+function calculateTotal() {
+  return Object.values(orders).reduce((total, item) => {
+    return total + (item.price * item.quantity)
   }, 0)
-  return document.querySelector('#order-total p').textContent = `£${total}`
 }
 
-function render(){
+function updateOrderTotal() {
+  const total = calculateTotal()
+  return orderTotalEl.textContent = `£${total}`
+}
+
+function renderMenu() {
   document.getElementById('menu-items').innerHTML = getMenuHtml(menuArray)
 }
 
-render()
+renderMenu()
