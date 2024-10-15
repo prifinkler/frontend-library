4a13f6e9c11643517dcbb2bcc4b7dc6edb9e24eb
Priscila Finkler <prifinkler@gmail.com>
2024-10-15 12:08:49 +0100

    added modal and thank you msg

--- restaurant-ordering/index.js
+++ restaurant-ordering/index.js
@@ -4,9 +4,12 @@ const orderSection = document.getElementById('order-section')
 const instructionsSection = document.getElementById('instructions-section')
 const orderItemsEl = document.getElementById('order-items')
 const orderTotalEl = document.querySelector('#order-total p')
-// const purchaseBtn = document.getElementById('purchase-btn')
+const mainEl = document.querySelector('main')
+const paymentModal = document.getElementById('payment-modal')
+const paymentForm = document.getElementById('payment-form')
+const paymentName = document.getElementById('name')
 
-const orders = {}
+let orders = {}
 
 document.addEventListener('click', function(e){
   const { add, remove } = e.target.dataset
@@ -14,8 +17,24 @@ document.addEventListener('click', function(e){
     handleAddClick(Number(add))
   } else if (remove) {
     handleRemoveClick(Number(remove))
+  } else if (e.target.id === 'complete-order-btn') {
+    handleCompleteOrder()
+  } else if (e.target.id === 'payment-modal-close-btn') {
+    handleCloseModal()
   }
   toggleOrderSection()
+})
+
+paymentForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  handleCloseModal()
+  orderSection.classList.add('hidden')
+  instructionsSection.classList.remove('hidden')
+  instructionsSection.classList.add('thanks-msg')
+  instructionsSection.innerHTML = `
+    <h3>Thanks, ${paymentName.value}! Your order is on its way!
+  `
+  orders = {}
 })
 
 function handleAddClick(itemId){
@@ -38,6 +57,16 @@ function handleRemoveClick(itemId) {
   return displayOrder()
 }
 
+function handleCompleteOrder() {
+  paymentModal.style.display = 'block'
+  mainEl.classList.add('modal-active')
+}
+
+function handleCloseModal() {
+  paymentModal.style.display = 'none'
+  mainEl.classList.remove('modal-active')
+}
+
 function toggleOrderSection(){
   const hasOrders = Object.keys(orders).length > 0
   orderSection.classList.toggle('hidden', !hasOrders)
@@ -55,7 +84,7 @@ function getMenuHtml(menuArr) {
               <p class="ingredients">${ingredients.join(', ')}</p>
               <p class="price">£${price}</p>
             </div>
-            <button id="add-btn" class="add-btn" data-add=${id}>
+            <button id="add-btn" class="add-btn" data-add=${id} aria-label="Add item">
               <i class="fa-solid fa-plus" data-add="${id}"></i>
             </button>
           </li>
@@ -68,8 +97,8 @@ function displayOrder() {
     return `
         <li class="order-item-info">
           <h3>${item.name} x ${item.quantity}</h3>
-          <button id="remove-btn" data-remove=${item.id}>remove</button>
-          <p>£${item.price * item.quantity}</p>
+          <button id="remove-btn" data-remove=${item.id} aria-label="Remove item">remove</button>
+          <p class="price">£${item.price * item.quantity}</p>
         </li>
     `
   }).join('')
